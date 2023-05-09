12a1e615fe042a5ec29b161f52e01b4560fba237
Luiti_LP <89021748+MeloCuentan@users.noreply.github.com>
2023-05-09 17:45:09 +0100

    Update EncoderMultifuncional.cpp

--- src/EncoderMultifuncional.cpp
+++ src/EncoderMultifuncional.cpp
@@ -73,7 +73,7 @@ void EncoderMultifuncional::detectarFlancoBajada(uint8_t pPin)
 /**
  * @brief Comprobar el estado del pulsador
  *
- * @param pin Número del pin a comprobar
+ * @param pin Pasamos el nombre del pin que queremos activar (PIN_A, PIN_B, PIN_C, PIN_D, PIN_SW)
  */
 bool EncoderMultifuncional::esPresionado(uint8_t pPin)
 {
@@ -103,8 +103,8 @@ bool EncoderMultifuncional::esPresionado(uint8_t pPin)
 /**
  * @brief Detección de cambio de flanco de cada pulsador
  *
- * @param pin Se le pasará el número de pin, entendiendo que PIN_A = 0, PIN_B = 1, PIN_C = 2, PIN_D = 3, PIN_SW = 4
- * @return int8_t Devuelve el cambio de pin o NULO sin o ha cambiado
+ * @param pin Se le pasará el nombre de pin (PIN_A, PIN_B, PIN_C, PIN_D, PIN_SW)
+ * @return int8_t Devuelve el cambio de pin o NULO si no ha cambiado
  */
 int8_t EncoderMultifuncional::detectarFlancos(int8_t pPin)
 {
@@ -165,11 +165,11 @@ void EncoderMultifuncional::cambiarLimites(int16_t pValorMinimo, int16_t pValorM
 }
 
 /**
- * @brief Cambiamos los límites del encoder y asignamos el valor que queramos del encoder. Este solo será cambiado si está dentro de los limites
+ * @brief Cambiamos los límites del encoder y asignamos el valor que queramos posicionar. Este solo será cambiado si está dentro de los limites
  *
  * @param valorMinimo Valor mínimo del encoder
  * @param valorMaximo Valor máximo del encoder
- * @param valorEncoder Asignamos un valor al encoder. Si esta valor no está dentro de los límites, se establece en el valor mínimo asignado
+ * @param valorEncoder Asignamos un valor al encoder. Si este valor no está dentro de los límites, se establece en el valor mínimo asignado
  */
 void EncoderMultifuncional::cambiarLimites(int16_t pValorMinimo, int16_t pValorMaximo, int16_t pValorEncoder)
 {
@@ -192,7 +192,6 @@ void EncoderMultifuncional::cambiarLimites(int16_t pValorMinimo, int16_t pValorM
 /**
  * @brief Comprueba si hay cambio y actualiza el último valor almacenado
  *
- * NOTE: Comprobar esta función. Las variables _contador y _contadorAnterior no tienen asignación
  * @return int16_t Delvuelve el valor del contador
  */
 int16_t EncoderMultifuncional::obtenerValor()
@@ -216,8 +215,7 @@ uint8_t EncoderMultifuncional::_read8()
 /**
  * @brief Cambiaremos el valor según el movimiento del encoder
  *
- * NOTE: Función en prueba. La función anterior está comentada justo debajo
- * @param accion Si es true, se suma el valor
+ * @param accion Si es true, se suma el valor. Si es false, se resta
  */
 void EncoderMultifuncional::_cambiarValor(bool pAccion)
 {
